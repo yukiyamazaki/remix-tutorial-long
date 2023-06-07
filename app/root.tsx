464fa746616a6ec77bf4370b129c05da437afd5a
YUKI YAMAZAKI <gooo0523@gmail.com>
2023-06-08 00:26:18 +0900

    feat:untile_prisma

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,22 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { LiveReload, Outlet, Links } from '@remix-run/react';
+import type { LinksFunction } from '@remix-run/node';
+
+import globalLargeStylesUrl from '~/styles/global-large.css';
+import globalMediumStylesUrl from '~/styles/global-medium.css';
+import globalStylesUrl from '~/styles/global.css';
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: 'stylesheet', href: globalStylesUrl },
+  {
+    rel: 'stylesheet',
+    href: globalMediumStylesUrl,
+    media: 'print, (min-width: 640px)',
+  },
+  {
+    rel: 'stylesheet',
+    href: globalLargeStylesUrl,
+    media: 'screen and (min-width: 1024px)',
+  },
 ];
 
 export default function App() {
@@ -19,13 +25,11 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
+        <title>Remix: So great, it's funny!</title>
         <Links />
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
